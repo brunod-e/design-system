f97c8f3beb5af49f6a653cb84b521d5d6e8eb4f8
Bruno Daniel Elias <brunodaniel.elias@gmail.com>
2023-02-24 10:21:44 -0300

    create box component

---
+++ packages/react/src/components/Box.tsx
@@ -0,0 +1,11 @@
+import { styled } from '../styles'
+import { ComponentProps } from 'react'
+
+export const Box = styled('div', {
+    padding: "$4",
+    borderRadius: "$md",
+    backgroundColor: "$gray800",
+    border: "1px solid $gray600",
+})
+
+export interface BoxProps extends ComponentProps<typeof Box> { }
