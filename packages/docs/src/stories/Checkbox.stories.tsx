e4282c1b3b93587be7a6bda9f4c464b4c55dcf08
Bruno Daniel Elias <brunodaniel.elias@gmail.com>
2023-02-27 10:41:11 -0300

    add checkbox stories

---
+++ packages/docs/src/stories/Checkbox.stories.tsx
@@ -0,0 +1,17 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { Box, Text, Checkbox, CheckboxProps } from '@ignite-ui/react';
+
+export default {
+  title: 'Form/Checkbox',
+  component: Checkbox,
+  decorators: [
+    (Story) => (
+      <Box as='label' css={{ display: 'flex', gap: '$2' }}>
+        {Story()}
+        <Text size='sm'>Accept terms of use</Text>
+      </Box>
+    ),
+  ],
+} as Meta<CheckboxProps>;
+
+export const Primary: StoryObj<CheckboxProps> = {};
