bab7ea95a9de69fc9148e4407f0f917b445a760d
Bruno D <brunodaniel.elias@gmail.com>
2023-12-14 15:15:02 -0300

    Create README.m

---
+++ README.m
@@ -0,0 +1,111 @@
+<br/>
+<p align="center">
+   <a href="https://github.com/brunod-e/ignite-call">
+    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/db/Npm-logo.svg/1200px-Npm-logo.svg.png" alt="Logo" width="80">
+  </a>
+
+  <h3 align="center">Design System </h3>
+
+  <p align="center">
+    Component library that contains a comprehensive set of guidelines, principles, and assets created using React and Storybook.
+    <br/>
+    <br/>
+    <a href="brunod-e.github.io/design-system/">View Demo</a>
+    .
+    <a href="https://github.com/brunod-e/design-system/issues">Report Bug</a>
+    .
+    <a href="https://github.com/brunod-e/design-system/issues">Request Feature</a>
+  </p>
+</p>
+
+![Downloads](https://img.shields.io/github/downloads/brunod-e/design-system/total) ![Contributors](https://img.shields.io/github/contributors/brunod-e/design-system?color=dark-green) ![Issues](https://img.shields.io/github/issues/brunod-e/design-system) ![License](https://img.shields.io/github/license/brunod-e/design-system)
+
+## Table Of Contents
+
+* [About the Project](#about-the-project)
+* [Built With](#built-with)
+* [Getting Started](#getting-started)
+  * [Prerequisites](#prerequisites)
+  * [Installation](#installation)
+* [Usage](#usage)
+* [Roadmap](#roadmap)
+* [Contributing](#contributing)
+* [Authors](#authors)
+* [Acknowledgements](#acknowledgements)
+
+## About The Project
+
+![Screen Shot](https://i.ibb.co/ng9pn5T/imagem-2023-12-12-190824874.png)
+
+The @brunod-e-ignite-ui/react is a React component library, providing a collection of UI components designed for seamless integration into React applications.
+
+The @brunod-e-ignite-ui/docs serves as documentation for the @brunod-e-ignite-ui library, providing comprehensive guidance and examples for developers. This documentation is a valuable resource for understanding the usage and features of the library.
+
+The @brunod-e-ignite-ui/tokens, part of the @brunod-e-ignite-ui library, provides a set of design tokens. Design tokens are variables that store visual design decisions, such as colors, typography, and spacing, ensuring consistency across a design system.
+
+## Built With
+
+React: Library for building user interfaces.<br/>
+Storybook:  An open-source tool for developing UI components in isolation.<br/>
+RadixUI: A collection of low-level UI primitives for building composable and accessible user interfaces.<br/>
+Typescript: Language for static typing.<br/>
+Stitches:  Library for styling React components.<br/>
+
+## Getting Started
+
+This project is available at: brunod-e.github.io/design-system/
+
+### Prerequisites
+
+* npm
+
+```sh
+npm install npm@latest -g
+```
+
+### Installation
+
+```sh
+npm install @brunod-e-ignite-ui
+```
+or
+```sh
+npm install @brunod-e-ignite-ui/react @brunod-e-ignite-ui/tokens
+```
+
+## Usage
+
+To use library components or tokens just import then from @brunod-e-ignite-ui:
+```sh
+import { Box, BoxProps, Text } from '@brunod-e-ignite-ui/react';
+```
+```sh
+import { colors } from '@brunod-e-ignite-ui/tokens';
+```
+
+## Roadmap
+
+See the [open issues](https://github.com/brunod-e/design-system/issues) for a list of proposed features (and known issues).
+
+## Contributing
+
+Contributions are what make the open source community such an amazing place to be learn, inspire, and create. Any contributions you make are **greatly appreciated**.
+* If you have suggestions for adding or removing projects, feel free to [open an issue](https://github.com/brunod-e/design-system/issues/new) to discuss it, or directly create a pull request after you edit the *README.md* file with necessary changes.
+* Please make sure you check your spelling and grammar.
+* Create individual PR for each suggestion.
+
+### Creating A Pull Request
+
+1. Fork the Project
+2. Create your Feature Branch (`git checkout -b feature/AmazingFeature`)
+3. Commit your Changes (`git commit -m 'Add some AmazingFeature'`)
+4. Push to the Branch (`git push origin feature/AmazingFeature`)
+5. Open a Pull Request
+
+## Authors
+
+* **Bruno D.** - *Fullstack Developer* - [Bruno D.](https://github.com/brunod-e) - *Built full library*
+
+## Acknowledgements
+
+* [Rocketseat](https://github.com/rocketseat-education)
